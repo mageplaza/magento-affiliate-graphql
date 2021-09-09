b3a525b8fcd72d4bae99c19e1e716d10670a9e9a
bruno <cuongpn@mageplaza.com>
2021-09-09 11:06:19 +0700

    add create refer link mutation

---
+++ Model/Resolver/Mutation/CreateReferLink.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_AffiliateGraphQl
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+declare(strict_types=1);
+
+namespace Mageplaza\AffiliateGraphQl\Model\Resolver\Mutation;
+
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\GraphQl\Model\Query\ContextInterface;
+
+/**
+ * Class CreateReferLink
+ * @package Mageplaza\AffiliateGraphQl\Model\Resolver
+ */
+class CreateReferLink extends AbstractAffiliate
+{
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        /** @var ContextInterface $context */
+        if (false === $context->getExtensionAttributes()->getIsCustomer()) {
+            throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized.'));
+        }
+
+        if (!$this->data->isEnabled()) {
+            throw new GraphQlAuthorizationException(__('The Affiliate is disabled.'));
+        }
+
+        $url = "";
+
+        if (isset($args['input']['refer_url'])) {
+            $url = $args['input']['refer_url'];
+        }
+
+        if (!$url) {
+            throw new GraphQlInputException(__('The refer url is required'));
+        }
+
+        $customer = $this->getCustomer->execute($context);
+        $affiliate = $this->data->getAffiliateAccount($customer->getId(), 'customer_id');
+
+        $param = $this->data->getSharingParam() . $affiliate->getId();
+
+        return $url . $param;
+    }
+}
